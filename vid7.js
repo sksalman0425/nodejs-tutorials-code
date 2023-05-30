4ca409a3e8e144aaf49499bc66800f4b786a2dfc
Salman Shaikh <sk.salman0425@gmail.com>
2023-05-30 15:12:14 +0530

    code improved

--- vid7.js
+++ vid7.js
@@ -1,17 +1,20 @@
 const http = require('http');
 
-http.createServer((req,resp) =>
-{
-resp.write("<h1>Code Step by step using nodemon which save time  of coder</h1>");
-resp.end();
-}
-).listen(4500);
-
+// http.createServer(serverFun).listen(4500);
 
 // function serverFun(req,resp){
 //     resp.write("<h1>Code Step by step using function</h1>");
 //    resp.end();
 
 // }
+//-----------------------------or--------------------------------------------
+// in createServer() we call another function in it or we can directly pass arrow function in it.
+
+http.createServer((req,resp) =>
+{
+resp.write("<h1>Code Step by step using nodemon which save time  of coder</h1>");
+resp.end();
+}
+).listen(4500);
 // run code at terminal and check output at below address
 //http://localhost:4500/
